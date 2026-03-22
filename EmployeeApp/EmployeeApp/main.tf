326b300edad7aab685b05658cf6892b4f7545937
Surendra N <51241646+surendranagireddi@users.noreply.github.com>
2026-03-22 12:37:03 +0530

    Create main.tf

---
+++ EmployeeApp/EmployeeApp/main.tf
@@ -0,0 +1,11 @@
+﻿# Configure the AWS Provider
+provider "aws" {
+  version = "~> 6.0"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = ami-0fb000fce1f78f9c7
+  instance_type = "t3.micro"
+
+}
