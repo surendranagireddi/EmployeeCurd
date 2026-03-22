76853bda31f689e659948fbb8e2463aea6728768
Surendra N <51241646+surendranagireddi@users.noreply.github.com>
2026-03-22 13:04:58 +0530

    updating main

--- EmployeeApp/EmployeeApp/main.tf
+++ EmployeeApp/EmployeeApp/main.tf
@@ -1,11 +1,17 @@
-﻿# Configure the AWS Provider
+﻿terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0" # Version goes here
+    }
+  }
+}
+
 provider "aws" {
-  version = "~> 6.0"
-  region  = "us-east-1"
+  region = "us-east-1" # Your region
 }
 
 resource "aws_instance" "example" {
-  ami           = ami-0fb000fce1f78f9c7
-  instance_type = "t3.micro"
-
+  ami           = "ami-0fb000fce1f78f9c7"
+  instance_type = "t2.micro"
 }
